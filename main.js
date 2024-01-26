7a550bb7b2d6ea46d50387d8a60d951074e9fcee
root <alexnewman42@gmail.com>
2024-01-26 08:35:12 -0500

    Format submit button and login link

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
+    let password = document.querySelector("#password").value;
+    let confirmPass = document.querySelector("#conf_password").value;
+    let error = document.querySelector("#error").value;
+
+    if(password == "" && confirmPass =="") {
+        error.innerHTML = "*passwords do not match"
+    }
+
+    if(password != confirmPassword) {
+
+    }
 })
